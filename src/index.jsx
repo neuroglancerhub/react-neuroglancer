b7cb4dd662b6d52d591931586bccf4b6aa574cbb
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2019-06-13 10:23:46 -0400

    Now handles arbitrary SegmentationUserLayers.

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { setupDefaultViewer } from '@janelia-flyem/neuroglancer';
 import React from 'react';
 import PropTypes from 'prop-types';
+import { SegmentationUserLayer } from '@janelia-flyem/neuroglancer/dist/module/neuroglancer/segmentation_user_layer';
 
 export default class Neuroglancer extends React.Component {
   constructor(props) {
@@ -14,6 +15,8 @@ export default class Neuroglancer extends React.Component {
     this.viewer = setupDefaultViewer({
       BrainMapsClientId: brainMapsClientId,
     });
+
+    this.viewer.layerManager.layersChanged.add(this.layersChanged);
 
     if (viewerState) {
       this.viewer.state.restoreState(viewerState);
@@ -36,8 +39,6 @@ export default class Neuroglancer extends React.Component {
       });
     }
 
-    this.setupChangedDispatch();
-
     // TODO: This is purely for debugging and we need to remove it.
     window.viewer = this.viewer;
   }
@@ -58,51 +59,63 @@ export default class Neuroglancer extends React.Component {
     );
   }
 
-  setupChangedDispatch = () => {
+  layersChanged = () => {
+    if (this.handlerRemovers) {
+      // If change handlers have been added already, call the function to remove each one,
+      // so there won't be duplicates when new handlers are added below.
+      this.handlerRemovers.forEach(remover => remover());
+    }
+
     if (this.viewer) {
       const { onSelectedChanged, onVisibleChanged } = this.props;
       if (onSelectedChanged || onVisibleChanged) {
-        const segmentationLayer = this.viewer.layerManager.getLayerByName('segmentation');
-        if (segmentationLayer) {
-          const { segmentSelectionState, visibleSegments } = segmentationLayer.layer.displayState;
-          if (segmentSelectionState && onSelectedChanged) {
-            segmentationLayer.registerDisposer(segmentSelectionState.changed.add(this.selectedChanged));
-          }
-          if (visibleSegments && onVisibleChanged) {
-            segmentationLayer.registerDisposer(visibleSegments.changed.add(this.visibleChanged));
+        this.handlerRemovers = [];
+
+        for (let layer of this.viewer.layerManager.managedLayers) {
+          if (layer.layer instanceof SegmentationUserLayer) {
+            const { segmentSelectionState, visibleSegments } = layer.layer.displayState;
+
+            if (segmentSelectionState && onSelectedChanged) {
+              // Bind the layer so it will be an argument to the handler when called.
+              const selectedChanged = this.selectedChanged.bind(undefined, layer);
+              const remover = segmentSelectionState.changed.add(selectedChanged);
+              this.handlerRemovers.push(remover);
+              layer.registerDisposer(remover);
+            }
+
+            if (visibleSegments && onVisibleChanged) {
+              const visibleChanged = this.visibleChanged.bind(undefined, layer);
+              const remover = visibleSegments.changed.add(visibleChanged);
+              this.handlerRemovers.push(remover);
+              layer.registerDisposer(remover);
+            }
           }
         }  
       }  
     }
   }
 
-  selectedChanged = () => {
+  selectedChanged = (layer) => {
     if (this.viewer) {
       const { onSelectedChanged } = this.props;
       if (onSelectedChanged) {
-        const segmentationLayer = this.viewer.layerManager.getLayerByName('segmentation');
-        if (segmentationLayer) {
-          const { segmentSelectionState } = segmentationLayer.layer.displayState;
-          if (segmentSelectionState) {
-            const segment = segmentSelectionState.hasSelectedSegment ? segmentSelectionState.selectedSegment : null;
-            onSelectedChanged(segment);
-          }
-        }  
+        const { segmentSelectionState } = layer.layer.displayState;
+        if (segmentSelectionState) {
+          const segment = segmentSelectionState.hasSelectedSegment ? segmentSelectionState.selectedSegment : null;
+          onSelectedChanged(segment, layer);
+        }
       }  
     }
   }
 
-  visibleChanged = () => {
+  visibleChanged = (layer) => {
     if (this.viewer) {
       const { onVisibleChanged } = this.props;
       if (onVisibleChanged) {
-        const segmentationLayer = this.viewer.layerManager.getLayerByName('segmentation');
-        if (segmentationLayer) {
-          const { visibleSegments } = segmentationLayer.layer.displayState;
-          if (visibleSegments) {
-            onVisibleChanged(visibleSegments);
-          }
-        }  
+        const { visibleSegments } = layer.layer.displayState;
+        if (visibleSegments) {
+          onVisibleChanged(visibleSegments, layer);
+        }
       }  
     }
   }
@@ -113,18 +126,22 @@ Neuroglancer.propTypes = {
   viewerState: PropTypes.object,
 
   /**
-   * A function of the form `(segment) => {}`, called each time there is a change to 
+   * A function of the form `(segment, layer) => {}`, called each time there is a change to 
    * the segment the user has "selected" (i.e., hovered over) in Neuroglancer.
    * The `segment` argument will be a Neuroglancer `Uint64` with the ID of the now-selected
    * segment, or `null` if no segment is now selected.
+   * The `layer` argument will be a Neuroglaner `ManagedUserLayer`, whose `layer` property 
+   * will be a Neuroglancer `SegmentationUserLayer`.
    */
   onSelectedChanged: PropTypes.func,
 
   /**
-   * A function of the form `(segments) => {}`, called each time there is a change to 
+   * A function of the form `(segments, layer) => {}`, called each time there is a change to 
    * the segments the user has designated as "visible" (i.e., double-clicked on) in Neuroglancer.
    * The `segments` argument will be a Neuroglancer `Uint64Set` whose elements are `Uint64`
    * instances for the IDs of the now-visible segments.
+   * The `layer` argument will be a Neuroglaner `ManagedUserLayer`, whose `layer` property 
+   * will be a Neuroglancer `SegmentationUserLayer`.
    */
   onVisibleChanged: PropTypes.func,
 };
